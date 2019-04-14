5e5b4ff0d9c2dca8f8839c21f055de51796a72c8
Amy Cen <amy.cen@blackrock.com>
2019-04-14 07:08:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.length == 1
+    array.to_s
+  elsif array.length == 2
     array.join(" and ")
     array[-1] = "and #{array[-1]}"
     array.join(", ")
